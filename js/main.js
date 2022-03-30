b153df546cdad225f2b96d2931f343193fff1602
khan-osama <osamaa.khan61@gmail.com>
2022-03-30 14:09:42 -0700

    Added event listener to update image src

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 /* global data */
 /* exported data */
+var imgURL = document.querySelector('#img-url');
+var img = document.querySelector('img');
+
+imgURL.addEventListener('input', function (event) {
+  img.setAttribute('src', imgURL.value);
+});
