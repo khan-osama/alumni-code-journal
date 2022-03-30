3a0d5155b725f7e642b52bb7d2924acfd5f62da5
Osama Khan <osamaa.khan61@gmail.com>
2022-03-30 15:00:09 -0700

    Revert "Issue 1 create an entry"

--- js/main.js
+++ js/main.js
@@ -1,26 +1,2 @@
 /* global data */
 /* exported data */
-var imgURL = document.querySelector('#url');
-var img = document.querySelector('img');
-var entryForm = document.querySelector('#entry-form');
-
-imgURL.addEventListener('input', function () {
-  img.setAttribute('src', imgURL.value);
-});
-
-entryForm.addEventListener('submit', function (event) {
-  event.preventDefault();
-
-  var userInputsObj = {
-    title: entryForm.elements.title.value,
-    photoURL: entryForm.elements.url.value,
-    notes: entryForm.elements.note.value,
-    nextEntryId: data.nextEntryId
-  };
-
-  data.nextEntryId = data.nextEntryId + 1;
-  data.entries.push(userInputsObj);
-
-  img.setAttribute('src', 'images/placeholder-image-square.jpg');
-  entryForm.reset();
-});
